5798197fd6ed5235cbc1ebbf8ea909e8dc21e826
burstknight <burstknight@gmail.com>
2021-12-02 14:04:57 +0800

    feat: Change button font
    Change button font.

--- src/myFrame/myWxFrame.py
+++ src/myFrame/myWxFrame.py
@@ -27,7 +27,7 @@ class myWxFrame ( wx.Frame ):
 		bSizer1.Add( self.m_oMessageTextField, 1, wx.ALL|wx.EXPAND, 5 )
 
 		self.m_oButton = wx.Button( self, wx.ID_ANY, u"Click me!", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_oButton.SetFont( wx.Font( 20, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, wx.EmptyString ) )
+		self.m_oButton.SetFont( wx.Font( 20, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "微軟正黑體" ) )
 
 		bSizer1.Add( self.m_oButton, 0, wx.ALL|wx.EXPAND, 5 )
 
@@ -55,5 +55,6 @@ class myWxFrame ( wx.Frame ):
 
 	def handleOnClickedButton( self, event ):
 		event.Skip()
+
 
 
