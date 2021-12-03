34164ead71ba88e74be76dc6f916fe1282a847de
burstknight <burstknight@gmail.com>
2021-12-03 09:39:44 +0800

    fix: Fix multi-thread show message box
    Fix the bug that GUI would crash because GUI lib dose not allow
    multi-thread  show message box or control GUI component in Linux.

--- src/myFrame/myFrame.py
+++ src/myFrame/myFrame.py
@@ -4,6 +4,25 @@ from myFrame.myWxFrame import myWxFrame
 from myObserverPattern import myInfoObserver, myInfo
 from myModel import myModel
 
+EVT_MSG_ID = wx.NewId()
+
+class myMsgBoxEvent(wx.PyEvent):
+    """
+    This event is degined for show message box
+    """
+    def __init__(self, strMessage:str):
+        wx.PyEvent.__init__(self)
+        self.SetEventType(EVT_MSG_ID)
+
+        self.__m_strMessage = strMessage
+    # End of myMsgBoxEvent
+
+    @property
+    def strMessage(self):
+        return self.__m_strMessage
+    # End of myMsgBoxEvent
+# End of class myMsgBoxEvent
+
 class myFrame(myWxFrame, myInfoObserver):
     def __init__(self, oModel:myModel):
         myWxFrame.__init__(self, None)
@@ -12,6 +31,7 @@ class myFrame(myWxFrame, myInfoObserver):
         self.__m_oModel = oModel
         self.__m_isRefresh = False
 
+        self.Connect(-1, -1, EVT_MSG_ID, self.handleOnMsgEvent)
         self.__m_vstrMessage = []
     # End of constructor
 
@@ -47,6 +67,11 @@ class myFrame(myWxFrame, myInfoObserver):
         # End of if-condition
     # End of myFrame::handleOnClickedButton
 
+    def handleOnMsgEvent(self, event:myMsgBoxEvent):
+        strMessage = event.strMessage
+        wx.MessageBox(strMessage, "Test")
+    # End of myFrame::handleOnMsgEvent
+
     def update(self, key: str, oInfo: myInfo):
         if "Message" == key:
             strMessage = oInfo.getParam(key)
@@ -58,7 +83,13 @@ class myFrame(myWxFrame, myInfoObserver):
             self.__m_isRefresh = True
         elif "Event" == key:
             strMessage = oInfo.getParam(key)
-            wx.MessageBox(strMessage, "Test")
+
+            """
+            Send event to this class and block the sender.
+            The sender will resume after the event is handled.
+            """
+            oEvent = myMsgBoxEvent(strMessage)
+            self.ProcessEvent(oEvent)
         # End of if-condition
     # End of myFrame::update
 # End of class myFrame
